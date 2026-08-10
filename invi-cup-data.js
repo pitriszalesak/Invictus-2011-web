b626b7b50dd624e1b1bba6a5c1c8a7863a010020
pitriszalesak <pitris.zalesak@gmail.com>
2026-08-10 08:27:09 +0200

    Přiřadit loga týmům Invi Cupu

--- invi-cup-data.js
+++ invi-cup-data.js
@@ -11,10 +11,10 @@ window.INVI_CUP_DATA = {
     loss: 0
   },
   teams: [
-    { id: "invictus", name: "Invictus 2011", shortName: "Invictus", seed: 1 },
-    { id: "slovan", name: "Slovan Havířov", shortName: "Slovan", seed: 2 },
+    { id: "invictus", name: "Invictus 2011", shortName: "Invictus", logo: "assets/logo-invictus-2011.png", logoType: "crest", seed: 1 },
+    { id: "slovan", name: "Slovan Havířov", shortName: "Slovan", logo: "assets/teams/slovan-havirov.png", logoType: "crest", seed: 2 },
     { id: "ludgerovice", name: "Ludgeřovice", shortName: "Ludgeřovice", seed: 3 },
-    { id: "lopata-1", name: "Joga Bonito", shortName: "Joga Bonito", seed: 4 },
+    { id: "lopata-1", name: "Joga Bonito", shortName: "Joga Bonito", logo: "assets/teams/joga-bonito.png", logoType: "crest", seed: 4 },
     { id: "lopata-2", name: "Lopata 2", shortName: "Lopata 2", seed: 5 }
   ],
   matches: [
